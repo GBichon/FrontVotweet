5e5d87b830879a33e444694602a02adb05632d4c
GBichon <bichon.german@gmail.com>
2017-05-14 04:44:47 -0400

    Rutas
    Rutas hechas, login funcionando, falta conectar a las vistas.

--- app/js/app.js
+++ app/js/app.js
@@ -4,12 +4,13 @@ var app= angular.module('myApp', ['ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'loginCtrl'});
   $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeCtrl'});
+  $routeProvider.when('/admin', {templateUrl: 'view/vistaAdministradorCandidatos.html'});
   $routeProvider.otherwise({redirectTo: '/login'});
 }]);
 
 
 app.run(function($rootScope, $location, loginService){
-	var routespermission=['/home'];  //route that require login
+	var routespermission=['/home','/admin'];  //route that require login
 	$rootScope.$on('$routeChangeStart', function(){
 		if( routespermission.indexOf($location.path()) !=-1)
 		{
